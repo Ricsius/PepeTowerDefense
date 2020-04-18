5f02f992e150317898a3daf7a4a2104f5f88cdbf
Hajnal Richárd <ci2e94@inf.elte.hu>
2020-04-18 23:31:16 +0200

    Update src/main/java/pepetd1/pkg0/BallistaTower.java, src/main/java/pepetd1/pkg0/ElementalTypeClass.java, src/main/java/pepetd1/pkg0/EntityWithAnimation.java, src/main/java/pepetd1/pkg0/FireTower.java, src/main/java/pepetd1/pkg0/GUI.java, src/main/java/pepetd1/pkg0/GameEngine.java, src/main/java/pepetd1/pkg0/Level.java files

--- src/main/java/pepetd1/pkg0/BallistaTower.java
+++ src/main/java/pepetd1/pkg0/BallistaTower.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package pepetd1.pkg0;
+package pepetd1.pkg0;
 
 import pepetd1.pkg0.Tower;
 import java.awt.Image;
