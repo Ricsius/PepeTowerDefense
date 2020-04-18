0f4c0143263cb0a3b2141d69739ba87ce39ef5e9
Hajnal Richárd <ci2e94@inf.elte.hu>
2020-04-18 20:19:49 +0200

    Upload New File

---
+++ src/pepetd1/pkg0/MouseListenerHandler.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepetd1.pkg0;
+
+import java.awt.*;  
+import java.awt.event.*;  
+import javax.swing.JFrame;
+
+public class MouseListenerHandler extends JFrame implements MouseListener{
+    private GameEngine engine;
+    
+    MouseListenerHandler(GameEngine ge){  
+        addMouseListener(this);
+        this.engine = ge;
+    }
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {  
+        engine.click(e.getX(), e.getY());
+        //System.out.println("Mouse");
+    }  
+    @Override
+    public void mouseEntered(MouseEvent e) {}  
+    @Override
+    public void mouseExited(MouseEvent e) {}  
+    @Override
+    public void mousePressed(MouseEvent e) {}  
+    @Override
+    public void mouseReleased(MouseEvent e) {}  
+      
+}  
