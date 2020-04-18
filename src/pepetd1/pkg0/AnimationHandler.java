a89bbc42334c22c7d0274d2c67628a8025c02243
Hajnal Richárd <ci2e94@inf.elte.hu>
2020-04-18 21:37:59 +0200

    Update AnimationHandler.java

--- src/pepetd1/pkg0/AnimationHandler.java
+++ src/pepetd1/pkg0/AnimationHandler.java
@@ -8,7 +8,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
+import java.io.IOException; tszgjrtj
 /**
  *
  * @author Hege
