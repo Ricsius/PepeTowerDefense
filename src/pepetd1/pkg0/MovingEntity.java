40617f2e06c43092bf50c057d4b8f3c4f368d00f
Hajnal Richárd <ci2e94@inf.elte.hu>
2020-04-18 20:20:00 +0200

    Upload New File

---
+++ src/pepetd1/pkg0/MovingEntity.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepetd1.pkg0;
+
+/**
+ *
+ * @author Hege
+ */
+public class MovingEntity extends EntityWithAnimation{
+    /*private int speed;
+
+    public MovingEntity(Position p){
+        super(p);
+    }
+
+    public void setSpeed(int s){
+        this.speed = s;
+    }
+
+    public void moveUp(){
+        int currentY = this.getPosition().getY();
+        this.getPosition().setY(currentY - speed);
+    }
+
+    public void moveDown(){
+        int currentY = this.getPosition().getY();
+        this.getPosition().setY(currentY + speed);
+    }
+
+    public void moveLeft(){
+        int currentX = this.getPosition().getX();
+        this.getPosition().setX(currentX - speed);
+    }
+
+    public void moveRight(){
+        int currentX = this.getPosition().getX();
+        this.getPosition().setX(currentX + speed);
+    }*/
+}
