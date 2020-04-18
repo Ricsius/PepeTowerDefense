fafd5447fd4814c70c029102a47a7ea0a9aa4398
Hajnal Richárd <ci2e94@inf.elte.hu>
2020-04-18 20:19:28 +0200

    Upload New File

---
+++ src/pepetd1/pkg0/Line.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepetd1.pkg0;
+
+import java.awt.Image;
+import java.io.IOException;
+
+/**
+ *
+ * @author Hege
+ */
+public class Line extends Sprite{
+    public Line(int x, int y, int width, int height, Image image) throws IOException{
+        super(x, y, width, height, image);
+    }
+    
+}
