33d726ef89842bf957c5055f42896dcca350f1f0
Hajnal Richárd <ci2e94@inf.elte.hu>
2020-04-18 20:21:49 +0200

    Upload New File

---
+++ src/pepetd1/pkg0/SonicPepe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepetd1.pkg0;
+
+import java.awt.Image;
+import java.io.IOException;
+
+/**
+ *
+ * @author Hege
+ */
+public class SonicPepe extends Pepe{
+    public SonicPepe(int x, int y, int width, int height, Image image, int health, int moneyReward) throws IOException{
+        super(x,y,width,height,image,health,moneyReward, "data/basicPepe2.png", 70, 90);
+    }
+}
