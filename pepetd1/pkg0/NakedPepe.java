ae54f8cf19a82b5809c5f7db0942910becc2ab02
Hajnal Richárd <ci2e94@inf.elte.hu>
2020-04-18 19:00:12 +0200

    Upload New File

---
+++ pepetd1/pkg0/NakedPepe.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepetd1.pkg0;
+
+import java.awt.Image;
+import java.io.IOException;
+
+/**
+ *
+ * @author Hege
+ */
+public class NakedPepe extends Pepe{
+    public NakedPepe(int x, int y, int width, int height, Image image, int health, int moneyReward) throws IOException{
+        super(x,y,width,height,image,health,moneyReward, "data/basicPepeFinal.png", 94, 100);
+    }
+}
