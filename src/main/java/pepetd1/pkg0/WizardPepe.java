7d77a4cef8eb8ef628f28fcf98d0a60464ee1003
Zsohár András <k39vmu@inf.elte.hu>
2020-04-22 12:10:45 +0200

    Update WizardPepe.java

--- src/main/java/pepetd1/pkg0/WizardPepe.java
+++ src/main/java/pepetd1/pkg0/WizardPepe.java
@@ -14,6 +14,6 @@ import java.io.IOException;
  */
 public class WizardPepe extends Pepe{
     public WizardPepe(int x, int y, int width, int height, Image image, int health, int moneyReward) throws IOException{
-        super(x,y,width,height,image,health,moneyReward, "data/basicPepe2.png", 70, 90);
+        super(x,y,width,height,image,health,moneyReward, "data/wizard.png", 72, 100);
     }
 }
