3e7cd5047d14c57e5d00c0fda8618d2e7f6a795c
Hegedüs Dominik <ob79wi@inf.elte.hu>
2020-05-13 21:45:12 +0200

    Update SonicPepe.java

--- src/main/java/pepetd1/pkg0/SonicPepe.java
+++ src/main/java/pepetd1/pkg0/SonicPepe.java
@@ -13,7 +13,7 @@ import java.io.IOException;
  * @author Hege
  */
 public class SonicPepe extends Pepe{
-    public SonicPepe(int x, int y, int width, int height, Image image, int health, int moneyReward) throws IOException{
-        super(x,y,width,height,image,health,moneyReward, "data/sonicpepe.png", 108, 100);
+    public SonicPepe(int x, int y, int width, int height, Image image) throws IOException{
+        super(x,y,width,height,image,130,30, "data/sonicpepe.png", 108, 100, ElementalType.ELECTRIC, ElementalType.ICE);
     }
 }
