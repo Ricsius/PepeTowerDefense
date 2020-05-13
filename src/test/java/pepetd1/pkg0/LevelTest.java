02184b60376b28c356ae548068dc8bce59530604
Lenkovics Marcell <r1nfeg@inf.elte.hu>
2020-05-13 21:55:11 +0200

    Update LevelTest.java

--- src/test/java/pepetd1/pkg0/LevelTest.java
+++ src/test/java/pepetd1/pkg0/LevelTest.java
@@ -15,100 +15,69 @@ public class LevelTest {
 	
 	@Test (expected = Exception.class)
 	public void testConstructor() throws IOException {
-		Level lvl = new Level("asd","asd");
+		Level lvl = new Level("asd","asd","asd");
 	}
 	
 	@Test
 	public void testConstructor2() throws IOException {
-		Level lvl = new Level("data/level_test1.txt","data/levelPepes_test.txt");
-			Assert.assertTrue("ures level",lvl.getSpots().isEmpty());
+		Level lvl = new Level("data/level_test1.txt","data/levelPepes_test.txt","data/level0move.txt");
+			Assert.assertTrue("Üres level",lvl.getSpots().isEmpty());
 	}
 	
 	@Test
 	public void testConstructor3() throws IOException {
-		Level lvl = new Level("data/level_test2.txt","data/levelPepes_test.txt");
+		Level lvl = new Level("data/level_test2.txt","data/levelPepes_test.txt","data/level0move.txt");
 			Assert.assertTrue("4 toronyhely",!lvl.getSpots().isEmpty());	
 	}
 	
 	@Test
 	public void testConstructor4() throws IOException {
-		Level lvl = new Level("data/level_test1.txt","data/levelPepes_test.txt");
-			boolean naked = lvl.getNakeds().isEmpty();
-			boolean wizard = lvl.getWizards().isEmpty();
-			boolean tank = lvl.getTanks().isEmpty();
-			boolean ricardo = lvl.getRicardo().isEmpty();
-			Assert.assertTrue("nincs pepe",naked && wizard && tank && ricardo);
+		Level lvl = new Level("data/level_test1.txt","data/levelPepes_test.txt","data/level0move.txt");
+			boolean pepes = lvl.getPepes().isEmpty();
+			Assert.assertTrue("nincs pepe",pepes);
 	}
 	
 	@Test
 	public void testConstructor5() throws IOException {
-		Level lvl = new Level("data/level_test1.txt","data/levelPepes1.txt");
-			int naked = lvl.getNakeds().size();
-			int wizard = lvl.getWizards().size();
-			int tank = lvl.getTanks().size();
-			int ricardo = lvl.getRicardo().size();
-			Assert.assertTrue("Vannak pepek",naked == 5 && wizard == 3 && tank == 2 && ricardo == 1);	
+		Level lvl = new Level("data/level_test1.txt","data/levelPepes1.txt","data/level0move.txt");
+			int pepes = lvl.getPepes().size();
+			Assert.assertTrue("Vannak pepek",pepes == 7);
 	}
 	
 	@Test
 	public void testTowerShoot() throws IOException {
-		Level lvl = new Level("data/level_test1.txt","data/levelPepes1.txt");
+		Level lvl = new Level("data/level_test1.txt","data/levelPepes1.txt","data/level0move.txt");
 			lvl.towerShoot();
 			Assert.assertTrue("Nincs torony",lvl.getBullets().isEmpty());		
 	}
 	
-	/*@Test
-	public void testTowerShoot2() throws IOException {
-		Level lvl = new Level("data/level_test3.txt","data/levelPepes.txt");
-		lvl.createBallistaTower(0, 0);
-			lvl.towerShoot();
-			Assert.assertTrue("Egy torony",lvl.getBullets().isEmpty());		
-	}*/
-	
-	/*@Test
-	public void testTowerShoot3() {
-		Level lvl = null;
-		try {
-			lvl = new Level("level_test2","levelPepes");
-			lvl.towerShoot();
-			Assert.assertTrue("T�bb torony",lvl.getBullets().size() == 12312);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}			
-	}*/
-	
-	/*@Test
-	public void testInRange1() {
-		
-	}*/
-	
 	@Test
 	public void testClickedInSpot1() throws IOException {
-		Level lvl = new Level("data/level_test1.txt","data/levelPepes_test.txt");
-		Assert.assertNull("Nincs torony hely",lvl.clickedInPlacementSpot(2, 3));
+		Level lvl = new Level("data/level_test1.txt","data/levelPepes_test.txt","data/level0move.txt");
+		Assert.assertNull("Nincs torony hely",lvl.clickedInPlacementSpot(2, 3, 0));
 	}
 	
 	@Test
 	public void testClickedInSpot2() throws IOException {
-		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt");
-		Assert.assertNull("Van torony hely, nem kattintunk bele",lvl.clickedInPlacementSpot(2, 3));
+		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt","data/level0move.txt");
+		Assert.assertNull("Van torony hely, nem kattintunk bele",lvl.clickedInPlacementSpot(2, 3, 0));
 	}
 	
 	@Test
 	public void testClickedInSpot2_2() throws IOException {
-		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt");
-		Assert.assertNotNull("Van torony hely, belekattintunk",lvl.clickedInPlacementSpot(131, 182));
+		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt","data/level0move.txt");
+		Assert.assertNotNull("Van torony hely, belekattintunk",lvl.clickedInPlacementSpot(131, 182, 0));
 	}
 	
 	@Test
 	public void testClickedInTower1() throws IOException {
-		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt");
+		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt","data/level0move.txt");
 		Assert.assertNull("Nincs torony",lvl.clickedInTower(131, 182));
 	}
 	
 	@Test
 	public void testClickedInTower2() throws IOException {
-		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt");
+		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt","data/level0move.txt");
 		lvl.getTowers().add(new Tower(0,0,null,0,0,0));
 		Assert.assertNull("Van torony, nem kattintunk bele",lvl.clickedInTower(131, 182));
 	}
@@ -117,7 +86,7 @@ public class LevelTest {
 	public void testClickedInTower2_2() {
 		Level lvl = null;
 		try {
-			lvl = new Level("data/level.txt","data/levelPepes_test.txt");
+			lvl = new Level("data/level.txt","data/levelPepes_test.txt","data/level0move.txt");
 			lvl.getTowers().add(new Tower(80,280,null,0,0,0));
 			Assert.assertNotNull("Van torony, belekattintunk",lvl.clickedInTower(133, 402));
 		} catch (IOException e) {
@@ -125,26 +94,19 @@ public class LevelTest {
 		}
 	}
 	
-	//csak IceTower-re n�zem meg, mivel a t�bbin�l is ugyanazt csin�lja a program
-	@Test
-	public void testCreateTower() throws IOException {
-		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt");
-		lvl.cutMoney(800);
-		Assert.assertNull("Nincs ra penz",lvl.createIceTower(0, 0));
-	}
 	
 	@Test
-	public void testCreateTower2() throws IOException {
-		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt");
-		Assert.assertNotNull("Van ra penz",lvl.createIceTower(0, 0));
+	public void testCreateTower() throws IOException {
+		Level lvl = new Level("data/level.txt","data/levelPepes_test.txt","data/level0move.txt");
+		Assert.assertNotNull("Van rá pénz",lvl.createIceTower(0, 0));
 	}
 	
 	@Test
 	public void testDeleteTower() throws IOException {
-		Level lvl = new Level("data/level_test1.txt","data/levelPepes_test.txt");
+		Level lvl = new Level("data/level_test1.txt","data/levelPepes_test.txt","data/level0move.txt");
 		lvl.createBallistaTower(0, 0);
 		lvl.deleteTower(new Tower(0,0,null,0,0,0));
-		Assert.assertTrue("Torlunk egy tornyot, helyere rakunk egy Spotot",lvl.getSpots().size() == 1);
+		Assert.assertTrue("Törlünk egy tornyot, helyére rakunk egy Spotot",lvl.getSpots().size() == 1);
 	}
 
 }
